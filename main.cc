68756d5c1af628dd4373d9bc5758c65109774cd2
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-09-08 17:12:27 +0530

    replaced `int` and `long long int` with `std::size_t`

--- main.cc
+++ main.cc
@@ -1,19 +1,17 @@
 #include <iostream>
 
 
+template <class T>
 /*
-
 Time complexity: O(log(n)), where n is the length of the array.
 
 Best Case: O(1), where 1 is the first index of the array.
 Wrost Case: O(log(n)), where n is the length of the array.
 Average / Expected Case: O(log(n)), where n is the length of the array.
 
-Works only when array is sorted and array contains only numbers (INT|FLOAT|DOUBLE|UNSIGNED|SIGNED|LONG).
-
+Works only when array is sorted and array contains only numbers.
 */
-template <class T>
-long long int BinarySearch(T array[], unsigned int size, T key)
+std::size_t BinarySearch(T array[], std::size_t size, T key)
 {
     unsigned int low = 0, high = size, mid;
     if (array[low] == key)
@@ -35,10 +33,10 @@ long long int BinarySearch(T array[], unsigned int size, T key)
 
 int main(void)
 {
-    int key;
-    int array[] = {1, 2, 3, 5, 6, 7, 8, 9, 10, 231, 1414, 3413, 5000, 12235, 15000, 17000, 19000, 20000};
+    std::size_t key;
+    std::size_t array[] = {1, 2, 3, 5, 6, 7, 8, 9, 10, 231, 1414, 3413, 5000, 12235, 15000, 17000, 19000, 20000};
     std::cout << ("enter key: ");
-    scanf("%d", &key);
-    printf("%d is at %d", key, BinarySearch<int>(array, sizeof(array) / sizeof(array[0]), key));
+    std::cin >> key;
+    printf("%d is at %d", key, BinarySearch<std::size_t>(array, sizeof(array) / sizeof(array[0]), key));
     return 0;
 }
